86235062460d7bcab9318b0d02f614fe1e3e184c
tobiasberger <wi24b052@technikum-wien.at>
2025-11-07 15:37:45 +0100

    add charging feature basics
    3 szenarios in feature file und step defs

---
+++ src/test/java/org/gruppeEins/StepDef_add_charging_point.java
@@ -0,0 +1,87 @@
+package org.gruppeEins;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDef_add_charging_point
+{
+    @Given("a location {string} exists in the system")
+    public void aLocationExistsInTheSystem(String arg0)
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("I add a charging point with identifier {string} of type {string} to {string}")
+    public void iAddAChargingPointWithIdentifierOfTypeTo(String arg0, String arg1, String arg2)
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("the charging point {string} is stored in the system")
+    public void theChargingPointIsStoredInTheSystem(String arg0)
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("its initial state is {string}")
+    public void itsInitialStateIs(String arg0)
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("it is visible under the location {string}")
+    public void itIsVisibleUnderTheLocation(String arg0)
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("a location {string} exists with a charging point {string}")
+    public void aLocationExistsWithAChargingPoint(String arg0, String arg1)
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("I try to add another charging point with identifier {string} to {string}")
+    public void iTryToAddAnotherChargingPointWithIdentifierTo(String arg0, String arg1)
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("the system rejects the request")
+    public void theSystemRejectsTheRequest()
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("I see an error message {string}")
+    public void iSeeAnErrorMessage(String arg0)
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("I try to add a charging point with identifier {string} of type {string} to the location {string}")
+    public void iTryToAddAChargingPointWithIdentifierOfTypeToTheLocation(String arg0, String arg1, String arg2)
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("the charging point is not created")
+    public void theChargingPointIsNotCreated()
+    {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
