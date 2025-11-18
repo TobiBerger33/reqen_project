8e26812b9d0da84ff9ef96cfc3b01b2e13313e2c
tobiasberger <wi24b052@technikum-wien.at>
2025-11-18 22:34:10 +0100

    implementation user story "register user"
    implemented four scenarios for feature "register user". adapted and expanded classes to add functionality and pass tests.

--- src/main/java/org/gruppeEins/Customer.java
+++ src/main/java/org/gruppeEins/Customer.java
@@ -1,16 +1,64 @@
 package org.gruppeEins;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Customer
 {
     private int id;
     private static int nextID = 1;
     private String name;
-    private double credit;
+    private String email;
+    private double credit = 0.00;
+    private static List<String> emailList= new ArrayList<>();
 
-    protected Customer(String name, double credit)
+    protected Customer(String name, String email)
     {
-        this.id = nextID++;
-        this.name = name;
-        this.credit = credit;
+        if (validInput(name, email) && validEmail(email) && availableEmail(email))
+        {
+            this.id = nextID++;
+            this.name = name;
+            this.email = email;
+            emailList.add(email);
+        }
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public double getCredit()
+    {
+        return credit;
+    }
+
+    private boolean validInput(String name, String email)
+    {
+        if(name.isEmpty() || email.isEmpty())
+        {
+            throw new IllegalArgumentException("Required information missing");
+        }
+
+        return true;
+    }
+
+    private boolean validEmail(String email)
+    {
+        if(!email.contains("@") || !email.contains("."))
+        {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+        return true;
+    }
+
+    private boolean availableEmail(String email)
+    {
+        if(emailList.contains(email))
+        {
+            throw new IllegalArgumentException("Email already registered");
+        }
+
+        return true;
     }
 }
