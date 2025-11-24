d8aa8c2feb773899f51146501e25d76b8ba9662a
tobiasberger <wi24b052@technikum-wien.at>
2025-11-24 18:57:39 +0100

    add feature and step definitions for viewing and updating charging station status

--- src/main/java/org/gruppeEins/Station.java
+++ src/main/java/org/gruppeEins/Station.java
@@ -3,12 +3,12 @@ package org.gruppeEins;
 public class Station
 {
     private int id;
-    protected static int nextID = 1;
+    private static int nextID = 1;
     private ChargingType type;
     private ChargingStatus status;
     private final Location location;
 
-    protected Station(ChargingType type, ChargingStatus status, Location location)
+    public Station(ChargingType type, ChargingStatus status, Location location)
     {
         this.id = nextID++;
         this.type = type;
@@ -16,18 +16,51 @@ public class Station
         this.location = location;
     }
 
-    protected void updateStatus(ChargingStatus status)
+    public Station(int id, ChargingType type, ChargingStatus status, Location location)
+    {
+        this.id = id;
+        this.type = type;
+        this.status = status;
+        this.location = location;
+    }
+
+    public ChargingStatus getStatus()
+    {
+        return status;
+    }
+
+    public void updateStatus(ChargingStatus status)
     {
         this.status = status;
     }
 
-    protected void updateType(ChargingType type)
+    public void updateType(ChargingType type)
     {
         this.type = type;
     }
 
-    protected Location getLocation()
+    public Location getLocation()
     {
         return location;
+    }
+
+    public String printStatus()
+    {
+        String statusTxt;
+
+        switch (status) {
+            case IN_OPERATION_FREE:
+                statusTxt = "IN OPERATION and FREE";
+                break;
+            case IN_OPERATION_OCCUPIED:
+                statusTxt = "IN OPERATION but OCCUPIED";
+                break;
+            default:
+                statusTxt = "OUT OF ORDER";
+        }
+
+        String message = "Station " + id + " is " + statusTxt;
+
+        return message;
     }
 }
